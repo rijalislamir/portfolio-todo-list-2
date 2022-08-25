f88a3c5bab144eeb67032f125ba893de976f8b31
Rijal Islami <rijalislamir@gmail.com>
2022-08-25 20:50:03 +0700

    Add router to the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import ActivityDetail from './pages/ActivityDetail'
+import Dashboard from './pages/Dashboard'
+import Login from './pages/Login'
+import Register from './pages/Register'
 
-function App() {
+const App = () => {
 	return (
-		<div>Hello World</div>
+		<Routes>
+			<Route path='/' element={<Dashboard />} />
+			<Route path='/login' element={<Login />} />
+			<Route path='/register' element={<Register />} />
+			<Route path='/activity/:id' element={<ActivityDetail />} />
+		</Routes>
 	)
 }
 
